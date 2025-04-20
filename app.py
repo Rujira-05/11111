c47a888caec57e5da14da278621bf1d83ae45c61
Rujira-05 <6531501168@lamduan.mfu.ac.th>
2025-04-20 14:41:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ Created on Sun Apr 20 13:52:05 2025
 """
 
 import streamlit as st
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, decode_predictions, preprocess_input
-from tensorflow.keras.preprocessing import image
+
 import numpy as np
 from PIL import Image
 import pickle
